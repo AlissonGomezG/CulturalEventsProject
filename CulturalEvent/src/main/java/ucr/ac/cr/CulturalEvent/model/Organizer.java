9cfef19214f43f0c42c57e93a1df996615a390b6
JohanMcBrain <johandrums09@gmail.com>
2025-04-20 17:27:41 -0600

    Modificado Organizer.java

--- CulturalEvent/src/main/java/ucr/ac/cr/CulturalEvent/model/Organizer.java
+++ CulturalEvent/src/main/java/ucr/ac/cr/CulturalEvent/model/Organizer.java
@@ -8,6 +8,52 @@ package ucr.ac.cr.CulturalEvent.model;
  *
  * @author Johan Brenes
  */
-public class Organizer {
-    
-}
+public class Organizer extends User {
+
+    private String biography;
+    private String contact;
+    private String events;
+
+    public Organizer() {
+        biography = "";
+        contact = "";
+        events = "";
+    }
+
+    public Organizer(String biography, String contact, String events, int id, String name, String email, String telephone, String userType) {
+        super(id, name, email, telephone, userType);
+        this.biography = biography;
+        this.contact = contact;
+        this.events = events;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getEvents() {
+        return events;
+    }
+
+    public void setEvents(String events) {
+        this.events = events;
+    }
+
+    @Override
+    public String toString() {
+        return "Organizer{" + "biography=" + biography + ", contact=" + contact + ", events=" + events + '}';
+    }
+
+}//endOrganizer
